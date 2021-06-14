fe3983e199c43cd4c98a39dfe9a35f53e7e24539
Paulo Roberto Torres <paulo.torres.apps@gmail.com>
2021-06-13 22:28:40 -0300

    Inputs das colunas csv corespondentes

--- resources/views/events/event/import_csv.blade.php
+++ resources/views/events/event/import_csv.blade.php
@@ -3,26 +3,33 @@
 
 @section('content')
 
-
-
-<div class="container-fluid h-100">
-    @foreach ($csv_head as $collum)
-        <div class="card card-primary card-outline">
-            <div class="card-header">
-            <h5 class="card-title">{{ $collum }}</h5>
-            </div>
-            <div class="card-body">
-                <select name="csv_head">
-                    <option value="1">Coluna 1</option>
-                    <option value="2">Coluna 2</option>
-                    <option value="3">Coluna 3</option>
-                    <option value="4">Coluna 4</option>
-                    <option value="5">Coluna 5</option>
-                    <option value="6">Coluna 6</option>
-                </select>
-            </div>
+<div class="card">
+    <div class="card-header border-0 bg-dark">
+      <h3 class="card-title">Configuração de Colunas</h3>
+      <div class="card-tools">
+        <a href="#" class="btn btn-sm btn-tool text-primary">
+          <i class="fas fa-save fa-2x"></i>
+        </a>
+      </div>
+    </div>
+    <div class="card-body">
+        <div class="container-fluid h-100">
+            @foreach ($csv_head as $collum)
+                <div class="form-group">
+                    <label>{{ $collum }}</label>
+                    <select class="form-control" name="csv_head">
+                        <option value="1">Coluna 1</option>
+                        <option value="2">Coluna 2</option>
+                        <option value="3">Coluna 3</option>
+                        <option value="4">Coluna 4</option>
+                        <option value="5">Coluna 5</option>
+                        <option value="6">Coluna 6</option>
+                    </select>
+                </div>
+                <hr>
+            @endforeach
         </div>
-    @endforeach
+    </div>
 </div>
 
 
