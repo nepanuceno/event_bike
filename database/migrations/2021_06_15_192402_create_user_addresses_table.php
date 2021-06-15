850a1da5525f3159425d7d78950734111df3da98
Paulo Roberto Torres <paulotorres@mpto.mp.br>
2021-06-15 16:38:16 -0300

    Add User  Address

---
+++ database/migrations/2021_06_15_192402_create_user_addresses_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_addresses', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->string('zip_code');
+            $table->string('address');
+            $table->integer('number');
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+
+
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_addresses');
+    }
+}
