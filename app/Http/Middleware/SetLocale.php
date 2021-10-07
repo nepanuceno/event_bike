ef55c84a4b0375a5c0c23f26c6e307b59c3f6079
Paulo Roberto Torres <paulotorres@mpto.mp.br>
2021-10-07 17:57:10 -0300

    Set Locale with HTTP_ACCEPT_LANGUAGE

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -17,6 +17,24 @@ class SetLocale
      */
     public function handle(Request $request, Closure $next)
     {
+        // dd($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+        if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+            preg_match_all('/([a-z]{1,8}(-[a-z]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?/i', $_SERVER['HTTP_ACCEPT_LANGUAGE'], $lang_parse);
+            if (count($lang_parse[1])){
+              $langs = array_combine($lang_parse[1], $lang_parse[4]);
+
+                foreach ($langs as $lang => $val){
+                  if ($val === ''){
+                      $langs[$lang] = 1;
+
+                      if ($lang =='pt-BR')
+                        App::setLocale('pt_BR');
+                  }
+                }
+                arsort($langs, SORT_NUMERIC);
+            }
+        }
+
         if (session()->get('language')) {
             App::setLocale(session()->get('language'));
         }
